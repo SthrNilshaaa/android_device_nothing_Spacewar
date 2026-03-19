0cafa351b7ff766fbf464ba4775991d0bfd4d53a
Samarth <smrth097@gmail.com>
2026-03-19 14:37:42 +0530

    BoardConfig.mk: Set TARGET_2ND_ARCH_VARIANT to armv8-2a
    Signed-off by: Samarth <smrth097@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,3 @@
-
 # Copyright (C) 2022-2025 The LineageOS Project	
 # 
 #
@@ -29,7 +28,7 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
